e6e1b14bef4f18ec4da6662d09b986108d90bbb2
Royal Finch <Royal2791@gmail.com>
2024-01-23 20:32:04 -0500

    add mouseover event listener to divs to recolor

--- app.js
+++ app.js
@@ -3,5 +3,10 @@ const container = document.querySelector('.container');
 for(let i = 0; i < 16; i++) {
     const div = document.createElement('div');
     div.classList.add(`'grid${i + 1}'`);
+    
+    div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'black';
+    });
+
     container.appendChild(div);
 }
