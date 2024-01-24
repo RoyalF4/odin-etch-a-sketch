3377d3ada046d31898713e55391804d5ebc82c6d
Royal Finch <Royal2791@gmail.com>
2024-01-24 16:06:23 -0500

    add button to redraw grid with size given by user

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const container = document.querySelector('.container');
-const containerSize = 500;
+const button = document.querySelector('button');
+const containerSize = 1000;
 let gridSize = 4;
+
+button.addEventListener('click', () => {
+    let newGridSize = prompt('Enter new grid size (max: 100):');
+    if(newGridSize > 100) newGridSize = 100;
+    container.textContent = '';
+    createGrid(containerSize, newGridSize);
+});
 
 function createGrid(containerSize, gridSize) {
 
-    const container = document.querySelector('.container');
-
     // fit container with grid size
-    container.style.width = Math.floor((containerSize / gridSize) * gridSize);
-    container.style.height = Math.floor((containerSize / gridSize) * gridSize);
+    container.style.width = containerSize / gridSize * gridSize;
+    container.style.height = containerSize / gridSize * gridSize;
 
     // create grid
     for(let i = 0; i < Math.pow(gridSize, 2); i++) {
         const div = document.createElement('div');
 
-        div.style.width = `${Math.floor(containerSize / gridSize)}px`;
-        div.style.height = `${Math.floor(containerSize / gridSize)}px`;
+        div.style.width = `${containerSize / gridSize}px`;
+        div.style.height = `${containerSize / gridSize}px`;
         
         div.addEventListener('mouseover', () => {
             div.style.backgroundColor = 'black';
