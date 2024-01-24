968ec4ca60a08cf3989cc2c98ea5abe7bb12c900
Royal Finch <Royal2791@gmail.com>
2024-01-24 14:47:25 -0500

    create a function to build the grid for any size

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const container = document.querySelector('.container');
+const containerSize = 500;
+let gridSize = 4;
 
-for(let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add(`'grid${i + 1}'`);
-    
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = 'black';
-    });
+function createGrid(containerSize, gridSize) {
 
-    container.appendChild(div);
+    const container = document.querySelector('.container');
+
+    // fit container with grid size
+    container.style.width = Math.floor((containerSize / gridSize) * gridSize);
+    container.style.height = Math.floor((containerSize / gridSize) * gridSize);
+
+    // create grid
+    for(let i = 0; i < Math.pow(gridSize, 2); i++) {
+        const div = document.createElement('div');
+
+        div.style.width = `${Math.floor(containerSize / gridSize)}px`;
+        div.style.height = `${Math.floor(containerSize / gridSize)}px`;
+        
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = 'black';
+        });
+
+        container.appendChild(div);
+    }
 }
+
+createGrid(containerSize, gridSize);
+
